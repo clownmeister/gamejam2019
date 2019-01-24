0ba3051688a7cfb08871f1e3da1ea59815113397
BuildTools <unconfigured@null.spigotmc.org>
2019-01-25 00:27:57 +0100

    First commit

---
+++ Assets/AbilityCoolDown.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class AbilityCoolDown : MonoBehaviour {
+
+	public string abilityButtonAxisName = "Fire1";
+	public Image darkMask;
+	public Text coolDownTextDisplay;
+
+	[SerializeField] private Ability ability;
+	[SerializeField] private GameObject weaponHolder;
+	private Image myButtonImage;
+	private AudioSource abilitySource;
+	private float coolDownDuration;
+	private float nextReadyTime;
+	private float coolDownTimeLeft;
+
+
+	void Start () 
+	{
+		Initialize (ability, weaponHolder); 
+	}
+
+	public void Initialize(Ability selectedAbility, GameObject weaponHolder)
+	{
+		ability = selectedAbility;
+		myButtonImage = GetComponent<Image> ();
+		abilitySource = GetComponent<AudioSource> ();
+		myButtonImage.sprite = ability.aSprite;
+		darkMask.sprite = ability.aSprite;
+		coolDownDuration = ability.aBaseCoolDown;
+		ability.Initialize (weaponHolder);
+		AbilityReady ();
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		bool coolDownComplete = (Time.time > nextReadyTime);
+		if (coolDownComplete) 
+		{
+			AbilityReady ();
---
+++ Assets/Spell.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Spell", menuName = "Spell/Basic", order = 1)]
+
+public class Spell : MonoBehaviour{
+
+	public float manaCost;
+
+	virtual public void Cast(Actor[] target, Actor castor){
+
+		//spell cast code goes here
+	}
+
+}
